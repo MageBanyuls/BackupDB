b02abfee5b100d75cb87370acd12cbf569974fa9
MageBanyuls <125486700+MageBanyuls@users.noreply.github.com>
2024-03-20 10:09:07 -0300

    translate comments main.py

--- main.py
+++ main.py
@@ -3,25 +3,25 @@ import os
 import datetime
 
 
-# Crear un cursor para ejecutar consultas SQL
+# Create cursor
 cursor = conexion.cursor()
 
-# Obtener el nombre de la base de datos
+# Name of db
 nombre_db = "miasesordb2"
 
-# Crear un directorio para almacenar los backups si no existe
+# Create dir for the back ups if it doesnt exit
 backup_dir = "backups"
 if not os.path.exists(backup_dir):
     os.makedirs(backup_dir)
 
-# Crear el nombre del archivo de respaldo
+# Create backup name
 fecha_actual = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
 nombre_archivo_backup = f"{nombre_db}_backup_{fecha_actual}.sql"
 ruta_archivo_backup = os.path.join(backup_dir, nombre_archivo_backup)
 
-# Abrir el archivo de respaldo
+# Open backup file
 with open(ruta_archivo_backup, 'w') as archivo:
-    # Obtener la estructura de las tablas y escribirla en el archivo
+    # Obtain tables structures
     cursor.execute("SHOW TABLES")
     for tabla in cursor.fetchall():
         tabla = tabla[0]
@@ -29,7 +29,7 @@ with open(ruta_archivo_backup, 'w') as archivo:
         create_table_query = cursor.fetchone()[1]
         archivo.write(f"{create_table_query};\n\n")
 
-        # Obtener y escribir los datos de las tablas en el archivo
+        # Obtain and write the data in the file
         cursor.execute(f"SELECT * FROM {tabla}")
         for fila in cursor.fetchall():
             valores = ', '.join([f"'{str(valor)}'" for valor in fila])
@@ -37,7 +37,7 @@ with open(ruta_archivo_backup, 'w') as archivo:
         archivo.write("\n")
 
 
-# Cerrar el cursor y la conexión
+# Close cursor and connection
 cursor.close()
 conexion.close()
 
